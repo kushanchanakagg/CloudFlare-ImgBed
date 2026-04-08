37dd2c732fd15f7d6f2f04e0128dcd577f00779c
kushanchanakagg <39917732+kushanchanakagg@users.noreply.github.com>
2026-04-08 15:25:57 +0530

    Delete functions/upload.js

--- functions/upload.js
+++
@@ -1,6 +0,0 @@
-import { onRequestPost as uploadHandler } from './upload/index.js';
-
-export async function onRequestPost(context) {
-  // forward POST request to the subfolder logic
-  return await uploadHandler(context);
-}
